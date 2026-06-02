14941f61d4ab0f8bbd9552c33a96c4d9bf4b0ea6
Emna MSALLEM <emsallem@IOTBOXDEV.LOCAL>
2026-06-02 20:27:02 +0100

    update prayer logic with countdown

--- app.js
+++ app.js
@@ -3,25 +3,71 @@ const axios = require("axios");
 
 const app = express();
 
-app.get("/", async (req, res) => {
+function getNextPrayer(timings) {
+  const now = new Date();
 
+  const prayers = [
+    { name: "Fajr", time: timings.Fajr },
+    { name: "Dhuhr", time: timings.Dhuhr },
+    { name: "Asr", time: timings.Asr },
+    { name: "Maghrib", time: timings.Maghrib },
+    { name: "Isha", time: timings.Isha },
+  ];
+
+  for (let p of prayers) {
+    const [h, m] = p.time.split(":");
+
+    const prayerTime = new Date();
+    prayerTime.setHours(h);
+    prayerTime.setMinutes(m);
+    prayerTime.setSeconds(0);
+
+    if (prayerTime > now) {
+      const diffMin = Math.floor((prayerTime - now) / 60000);
+
+      return {
+        name: p.name,
+        minutes: diffMin
+      };
+    }
+  }
+
+  // si toutes les prières sont passées → demain Fajr
+  const [h, m] = timings.Fajr.split(":");
+  const prayerTime = new Date();
+  prayerTime.setDate(prayerTime.getDate() + 1);
+  prayerTime.setHours(h);
+  prayerTime.setMinutes(m);
+
+  const diffMin = Math.floor((prayerTime - now) / 60000);
+
+  return {
+    name: "Fajr",
+    minutes: diffMin
+  };
+}
+
+app.get("/", async (req, res) => {
   const response = await axios.get(
     "https://api.aladhan.com/v1/timingsByCity?city=Tunis&country=Tunisia"
   );
 
   const timings = response.data.data.timings;
 
+  const next = getNextPrayer(timings);
+
   res.json({
     frames: [
       {
-        text: `🕌 Fajr ${timings.Fajr}`
+        text: `🕌 ${next.name}`
+      },
+      {
+        text: `⏳ dans ${next.minutes} min`
       }
     ]
   });
-
 });
 
-// IMPORTANT Render utilise process.env.PORT
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
